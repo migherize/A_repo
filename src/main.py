f999b72e0067e1a6dd1170387ee9be6a449708b9
migherize <migherize@gmail.com>
2022-07-29 00:06:34 -0400

    Codigo en main para uso de repositorio commons

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from common_repo.commons.schemas import Persona, Animal
+
+P1 = Persona(25832303, "Miguel","Herize","M")
+print("Dueño: ",P1.cedula)
+
+M1 = Animal(25832303, "Tommy","Perro","M")
+print("Mascota: ",M1.nombre)
